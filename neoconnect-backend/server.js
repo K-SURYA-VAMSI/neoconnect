41a0547900cc4242f3f2d6d075242f5d5821634d
K-SURYA-VAMSI <ksurya1104@gmail.com>
2026-03-16 00:43:03 +0530

    last commit

--- neoconnect-backend/server.js
+++ neoconnect-backend/server.js
@@ -13,7 +13,7 @@ require("./jobs/escalationJob");
 const app = express();
 
 app.use(cors({
- origin: "http://localhost:3000",
+ origin: process.env.FRONTEND_URL,
  credentials: true
 }));
 app.use(express.json());
